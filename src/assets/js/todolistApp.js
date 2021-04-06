2857becdf587c9b3124eb9f04afd638456f328b3
Yury Kuzmich <yurakuzmich@gmail.com>
2021-04-06 22:07:25 +0300

    feat: try to use localstorage

--- src/assets/js/todolistApp.js
+++ src/assets/js/todolistApp.js
@@ -1,4 +1,4 @@
-import { tasks } from './mocktasks';
+
 import { tasksCategories } from './taskscategories';
 
 import { AddTaskForm } from './addtaskform';
@@ -12,28 +12,35 @@ export class ToDoListApp {
     constructor(parentElement,) {
         this.parentElement = parentElement;
         this.tasksBoxes = [];
-        this.tasks = [];
+        // this.tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+        if(JSON.parse(localStorage.getItem('tasks')) === null) {
+            this.tasks = [];
+        } else {
+            this.tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+
+        console.log(this.tasks);
+
         this.init(this.parentElement);
 
         this.addTaskForm = new AddTaskForm(parentElement, this.tasksCategories);
         this.taskListElement = new TaskList(parentElement);
-
-        this.renderCategories();
 
         this.createTasksBoxes(this.tasksCategories);
 
         this.renderTasksBoxes();
 
         document.addEventListener('click', (e) => {
-            if(e.target === this.addTaskForm.taskSubmit) {
+            if (e.target === this.addTaskForm.taskSubmit) {
                 this.addTask(this.addTaskForm.taskInput.value, this.addTaskForm.categorySelection.value);
             }
 
             this.tasks.forEach((task) => {
-                if(e.target === task.deleteTaskElement) {
+                if (e.target === task.deleteTaskElement) {
                     e.preventDefault();
-                    console.log(`Cross clicked`);
                     task.taskElement.remove();
+                    console.log(`task id = `, task.id);
+                    this.deleteTaskFromStorage(task.id);
                 }
             });
         });
@@ -42,7 +49,6 @@ export class ToDoListApp {
     init(targetElement) {
         this.createAppWrapper(targetElement);
         this.loadTasksCategories(tasksCategories)
-        this.loadTasks(tasks);
     };
 
     createAppWrapper(targetElement) {
@@ -51,18 +57,8 @@ export class ToDoListApp {
         targetElement.append(this.appWrapper);
     }
 
-    loadTasks(tasklist) {
-        this.taskList = tasklist;
-    }
-
     loadTasksCategories(tasksCategories) {
         this.tasksCategories = tasksCategories;
-    }
-
-    renderCategories() {
-        this.tasksCategories.forEach(category => {
-            console.log(category.id + ':' + category.title);
-        });
     }
 
     createTasksBoxes(categories) {
@@ -73,30 +69,39 @@ export class ToDoListApp {
 
     renderTasksBoxes() {
         this.tasksBoxes.forEach(taskBox => {
-            let currentTaskList = this.taskList.filter(task => task.category === taskBox.category);
+            let currentTaskList = this.tasks.filter(task => task.category === taskBox.category);
             currentTaskList.forEach(task => {
-                this.tasks = [...this.tasks, new Task(task.id, task.title, task.category, task.status, taskBox.listElement, this.deleteTask)];
+                this.tasks = [...this.tasks, new Task(task.id, task.title, task.category, task.status, taskBox.listElement)];
             });
         });
+        this.updateLocalStorage();
     }
 
     addTask(task, category) {
         if (task.length < 5) {
             alert(`Enter minimum 5 symbols`);
+            return;
         } else {
             let newId = Date.now() + Math.round(100 * Math.random());
-            this.tasks = [...this.tasks, new Task(newId, task, 0, category, this.tasksBoxes[category].listElement)];
+            this.tasks = [...this.tasks, new Task(newId, task, category, 0, this.tasksBoxes[category].listElement)];
         }
         this.addTaskForm.taskInput.value = '';
+        this.updateLocalStorage();
     }
 
-    deleteTask(e, taskId, domElement ) {
-        e.preventDefault();
-        domElement.remove();
-        console.log(this);
+    deleteTaskFromStorage(taskId) {
+        this.tasks = this.tasks.filter((task) => {
+            console.log(task.id !== taskId);
+            let flag = task.id !== taskId;
+            return flag;
+        });
+        this.updateLocalStorage();
     }
 
     updateLocalStorage() {
-            localStorage.setItem('tasks', this.tasks);
+        
+        console.log(`update storage`);
+        localStorage.removeItem('tasks');
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
